eb3f7ae9d275bd9c6721587ceabfc49565ff3d9f
siskaand209 <siskakamilfb@gmail.com>
2018-04-01 23:43:36 +0700

    ang 5 01/04/

--- XKitchenAngular5/src/app/services/table.service.ts
+++ XKitchenAngular5/src/app/services/table.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@angular/core';
 import { Table } from '../models/table.model';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+import { User } from '../models/user.model';
 
 @Injectable()
 export class TableService {
@@ -9,10 +13,16 @@ export class TableService {
   private _url: 'http://localhost:3000/api/tables';
 
   selectedTable: Table;
-  constructor(private http: HttpClient) { }
+  tableList: Table[];
+  constructor(private http: Http) { }
 
-  getTable(): Observable<Table[]> {
-    return this.http.get<Table[]>(this._url);
+  getTables() {
+    this.http.get(this._url) 
+    .map((data: Response) => {
+      return data.json() as Table[];
+    }).toPromise().then(x => {
+      this.tableList = x;
+    });
   }
 
 }
